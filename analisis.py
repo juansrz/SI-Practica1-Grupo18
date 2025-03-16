7f94067fc5fbc7ee8ad9dca517e61ae3525d8b60
Juan Antonio Suárez Suárez <95307238+juansrz@users.noreply.github.com>
2025-03-16 12:45:53 +0100

    Fix analisis.py
    - Corrección

--- analisis.py
+++ analisis.py
@@ -6,6 +6,7 @@ from datetime import datetime
 conn = sqlite3.connect("incidencias.db")
 df_tickets = pd.read_sql_query("SELECT * FROM tickets", conn)
 df_contactos = pd.read_sql_query("SELECT * FROM contactos_empleados", conn)
+df_empleados = pd.read_sql_query("SELECT * FROM empleados", conn)
 conn.close()
 
 # 2) Convertimos las columnas de fecha a tipo datetime
@@ -81,3 +82,122 @@ print(f"Media de horas por incidencia: {media_horas:.2f}, Desv: {std_horas:.2f}"
 print(f"Mín de horas totales por empleado: {min_horas:.2f}, Máx: {max_horas:.2f}")
 print(f"Mín de duración (días) de un ticket: {min_duracion}, Máx: {max_duracion}")
 print(f"Mín # incidencias atendidas por un empleado: {min_incid_emp}, Máx: {max_incid_emp}")
+
+
+
+
+# -----------------------------------------------------------------------------
+#                                   Agrupaciones
+# -----------------------------------------------------------------------------
+
+pd.set_option('display.max_columns', None)
+pd.set_option('display.width', None)
+
+df_fraude = df_tickets[df_tickets["tipo_incidencia"] == '5']
+print(f"Numero de incidencias de fraude: {len(df_fraude)}\n")
+
+# 1) Agrupaciones y calculos de incidencias y actuaciones en caso de fraude por empleado
+
+group_fraude = df_contactos[df_contactos["id_ticket"].isin(df_fraude["id_ticket"])]
+
+group_empleado = group_fraude.groupby("id_emp")["id_ticket"].count().reset_index(name="Incidencias")
+group_empleado_actuaciones = group_fraude.groupby("id_emp")["id_ticket"].nunique().reset_index(name="Actuaciones")
+group_empleado = group_empleado.merge(group_empleado_actuaciones, on="id_emp", how="left")
+
+# Estadisticas basicas sobre el numero de incidencias de fraude por empleado
+mediana_empleado = group_empleado["Incidencias"].median()
+media_empleado = group_empleado["Incidencias"].mean()
+varianza_empleado = group_empleado["Incidencias"].var()
+max_empleado = group_empleado["Incidencias"].max()
+min_empleado = group_empleado["Incidencias"].min()
+
+print(f"Agrupacion por Empleado:\n{group_empleado}\n")
+print(f"Estadisticas basicas por nivel de empleado:")
+print(f"Mediana: {media_empleado}, Media: {mediana_empleado}, Varianza: {varianza_empleado}, Max: {max_empleado}, Min: {min_empleado}\n")
+
+
+# 2) Agrupaciones y calculos de incidencias y actuaciones en caso de fraude por nivel de empleado
+
+group_nivel = group_fraude.groupby("id_emp")["id_ticket"].count().reset_index(name="Incidencias")
+group_nivel_actuaciones = group_fraude.groupby("id_emp")["id_ticket"].nunique().reset_index(name="Actuaciones")
+group_nivel = group_nivel.merge(group_nivel_actuaciones, on="id_emp", how="left")
+group_nivel = group_nivel.merge(df_empleados[["id_emp", "nivel"]], on="id_emp", how="left")
+group_nivel = group_nivel.groupby("nivel", as_index=False)[["Incidencias", "Actuaciones"]].sum()
+
+# Estadisticas basicas sobre el numero de incidencias de fraude por nivel de empleado
+mediana_nivel = group_empleado["Incidencias"].median()
+media_nivel = group_nivel["Incidencias"].mean()
+varianza_nivel = group_nivel["Incidencias"].var()
+max_nivel = group_nivel["Incidencias"].max()
+min_nivel = group_nivel["Incidencias"].min()
+
+print(f"Agrupacion por nivel de empleado:\n{group_nivel}\n")
+print(f"Estadisticas basicas por nivel de empleado:")
+print(f"Mediana: {mediana_nivel}, Media: {media_nivel}, Varianza: {varianza_nivel}, Max: {max_nivel}, Min: {min_nivel}\n")
+
+
+# 3) Agrupaciones y calculos de incidencias y actuaciones en caso de fraude por cliente
+
+group_cliente = df_fraude.groupby("cliente")["id_ticket"].count().reset_index(name="Incidencias")
+group_cliente_actuaciones = group_fraude.merge(df_tickets[["id_ticket", "cliente"]], on="id_ticket", how="left")
+group_cliente_actuaciones = group_cliente_actuaciones.groupby("cliente")["id_emp"].count().reset_index(name="Actuaciones")
+group_cliente = group_cliente.merge(group_cliente_actuaciones, on="cliente", how="left")
+
+
+# Estadisticas basicas sobre el numero de incidencias de fraude por cliente
+mediana_cliente = group_cliente["Incidencias"].median()
+media_cliente = group_cliente["Incidencias"].mean()
+varianza_cliente = group_cliente["Incidencias"].var()
+max_cliente = group_cliente["Incidencias"].max()
+min_cliente = group_cliente["Incidencias"].min()
+
+
+print(f"Agrupacion por cliente:\n{group_cliente}\n")
+print(f"Estadisticas basicas por cliente:")
+print(f"Mediana: {mediana_cliente}, Media: {media_cliente}, Varianza: {varianza_cliente}, Max: {max_cliente}, Min: {min_cliente}\n")
+
+
+# 4) Agrupaciones y calculos de incidencias y actuaciones en caso de fraude por tipo de incidencia
+
+group_incidencia = df_fraude.groupby("tipo_incidencia")["id_ticket"].count().reset_index(name="Incidencias")
+group_incidencia_actuaciones = df_contactos.merge(df_fraude[["id_ticket", "tipo_incidencia"]], on="id_ticket", how="inner")
+group_incidencia_actuaciones = (group_incidencia_actuaciones.groupby("tipo_incidencia")["id_emp"].count().reset_index(name="Actuaciones"))
+group_incidencia = group_incidencia.merge(group_incidencia_actuaciones, on="tipo_incidencia", how="left")
+
+
+
+# Estadisticas basicas sobre el numero de incidencias de fraude por tipo de incidencia
+mediana_incidencia = group_incidencia["Incidencias"].median()
+media_incidencia= group_incidencia["Incidencias"].mean()
+varianza_incidencia = group_incidencia["Incidencias"].var()
+max_incidencia = group_incidencia["Incidencias"].max()
+min_incidencia = group_incidencia["Incidencias"].min()
+
+print(f"Agrupacion por tipo de incidencia:\n{group_incidencia}\n")
+print(f"Estadisticas basicas por tipo de incidencia:")
+print(f"Mediana: {mediana_incidencia}, Media: {media_incidencia}, Varianza: {varianza_incidencia}, Max: {max_incidencia}, Min: {min_incidencia}\n")
+
+
+# 5) Agrupaciones y calculos de incidencias y actuaciones en caso de fraude dia de la semana
+
+group_fraude = df_contactos[df_contactos["id_ticket"].isin(df_fraude["id_ticket"])].copy()
+group_fraude["dia_semana"] = group_fraude["fecha"].dt.dayofweek
+
+group_dia = group_fraude.groupby("dia_semana")["id_ticket"].count().reset_index(name="Incidencias")
+group_dia_actuaciones = group_fraude.groupby("dia_semana")["id_ticket"].nunique().reset_index(name="Actuaciones")
+group_dia = group_dia.merge(group_dia_actuaciones, on="dia_semana", how="left")
+
+
+# Estadisticas basicas sobre el numero de incidencias de fraude por día de la semana
+mediana_dia = group_dia["Incidencias"].median()
+media_dia = group_dia["Incidencias"].mean()
+varianza_dia = group_dia["Incidencias"].var()
+max_dia = group_dia["Incidencias"].max()
+min_dia = group_dia["Incidencias"].min()
+
+print(f"Agrupacion por dia de la semana:\n{group_dia}\n")
+print(f"Estadisticas basicas por dia de la semana:")
+print(f"Mediana: {mediana_dia}, Media: {media_dia}, Varianza: {varianza_dia}, Max: {max_dia}, Min: {min_dia}\n")
+
+conn.close()
+
